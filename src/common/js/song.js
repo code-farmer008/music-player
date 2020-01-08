3882fefaaa21dfcd2e6641e5a915857f3e64900e
code-farmer008 <59639839+code-farmer008@users.noreply.github.com>
2020-01-08 15:57:18 +0800

    Add files via upload

---
+++ src/common/js/song.js
@@ -0,0 +1,70 @@
+import {commonParams} from '../../api/config'
+import axios from 'axios'
+import {Base64} from 'js-base64'
+
+export default class Song {
+  constructor({id, mid, singer, name, album, duration, image}) {
+    this.id = id
+    this.mid = mid
+    this.singer = singer
+    this.name = name
+    this.album = album
+    this.duration = duration
+    this.image = image
+  }
+
+  getLyric() {
+    if (this.lyric) {
+      return Promise.resolve(this.lyric)
+    }
+
+    return new Promise((resolve, reject) => {
+      const url = '/api/lyric'
+
+      const data = Object.assign({}, commonParams, {
+        songmid: this.mid,
+        platform: 'yqq',
+        hostUin: 0,
+        needNewCode: 0,
+        categoryId: 10000000,
+        pcachetime: +new Date(),
+        format: 'json'
+      })
+
+      axios.get(url, {
+        params: data
+      }).then(res => {
+        if(res.status == 200 && res.data.code == 0){
+          this.lyric = Base64.decode(res.data.lyric.lyric);
+          resolve(this.lyric)
+        }else{
+          reject('no lyric')
+        }
+      })
+    })
+  }
+}
+
+export function createSong(musicData) {
+  return new Song({
+    id: musicData.songid,
+    mid: musicData.songmid,
+    singer: filterSinger(musicData.singer),
+    name: musicData.songname,
+    album: musicData.albumname,
+    duration: musicData.interval,
+    image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`
+  })
+}
+
+function filterSinger(singer) {
+  let ret = []
+  if (!singer) {
+    return ''
+  }
+  singer.forEach((s) => {
+    ret.push(s.name)
+  })
+  return ret.join('/')
+}
+
