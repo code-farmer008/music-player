462ca167a01089273436d4b1ccff73d4fca1c3cd
code-farmer008 <59639839+code-farmer008@users.noreply.github.com>
2020-01-08 15:36:28 +0800

    Create m-header.vue

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="m-header">
+    <span class="icon"></span>
+    <h1 class="text">Chicken Music</h1>
+    <router-link tag="div" class="mine" to="/user">
+      <i class="icon-mine"></i>
+    </router-link>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped lang="stylus" >
+  
+  @import "common/stylus/variable.styl"
+  @import "common/stylus/mixin.styl"
+  .m-header
+    height: 44px
+    text-align: center
+    color: $color-theme
+    .icon
+      display: inline-block
+      vertical-align: top
+      margin:6px 9px 0 0
+      width: 30px
+      height: 32px
+      bg-image('logo')
+      background-size: 30px 32px
+    .text
+      display: inline-block
+      vertical-align: top
+      line-height: 44px
+      font-size: $font-size-large
+    .mine
+      position: absolute
+      top: 0
+      right: 0
+      .icon-mine
+        display: block
+        padding: 12px
+        font-size: 20px
+        color: $color-theme
+</style>
